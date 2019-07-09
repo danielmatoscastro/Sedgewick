d3cbe4482b0220fd166ee3974a769675008f06be
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2019-07-09 17:18:44 -0300

    Solução do exercício 1.1.33

---
+++ Capitulo1/Capitulo1/Exercicio1133_Cliente.cs
@@ -0,0 +1,179 @@
+using System;
+using Capitulo1_Biblioteca;
+
+namespace Capitulo1_Cliente
+{
+    /// <summary>
+    /// Classe cliente para o exercício 1.1.33.
+    /// </summary>
+    public class Exercicio1133_Cliente
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ApresentarMenu();
+
+                int opcao = LerOpcao();
+
+                ExecutarOperacaoEscolhida(opcao);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static void ExecutarOperacaoEscolhida(int opcao)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    ProdutoEscalar();
+                    break;
+
+                case 2:
+                    ProdutoMatrizMatriz();
+                    break;
+
+                case 3:
+                    Transposta();
+                    break;
+
+                case 4:
+                    ProdutoMatrizVetor();
+                    break;
+
+                case 5:
+                    ProdutoVetorMatriz();
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+
+        private static int LerOpcao()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static void ApresentarMenu()
+        {
+            Console.WriteLine("Olá. Escolha a opção desejada.");
+            Console.WriteLine("1. Produto escalar.");
+            Console.WriteLine("2. Produto matriz-matriz.");
+            Console.WriteLine("3. Transposta de uma matríz.");
+            Console.WriteLine("4. Produto matriz-vetor.");
+            Console.WriteLine("5. Produto vetor-matriz.");
+        }
+
+        private static void ProdutoVetorMatriz()
+        {
+            Console.WriteLine("Insira o vetor:");
+            double[] vetor = LerVetor();
+
+            Console.WriteLine("Insira a matriz:");
+            double[][] matriz = LerMatriz();
+
+            double[] resultado = Matrix.mult(vetor, matriz);
+
+            Console.WriteLine("Resultado: ");
+            Console.WriteLine(ImpressorVetorial.ImprimeArray<double>(resultado));
+        }
+
+        private static void ProdutoMatrizVetor()
+        {
+            Console.WriteLine("Insira a matriz:");
+            double[][] matriz = LerMatriz();
+
+            Console.WriteLine("Insira o vetor:");
+            double[] vetor = LerVetor();
+
+            double[] resultado = Matrix.mult(matriz, vetor);
+
+            Console.WriteLine("Resultado: ");
+            Console.WriteLine(ImpressorVetorial.ImprimeArray<double>(resultado));
+        }
+
+        private static void Transposta()
+        {
+            Console.WriteLine("Insira a matriz:");
+            double[][] matriz = LerMatriz();
+
+            double[][] resultado = Matrix.Transpose(matriz);
+
+            Console.WriteLine("Resultado:");
+            Console.WriteLine(ImpressorMatricial.ImprimeMatriz<double>(resultado));
+        }
+
+        private static void ProdutoMatrizMatriz()
+        {
+            Console.WriteLine("Insira a primeira matriz:");
+            double[][] matrizA = LerMatriz();
+
+            Console.WriteLine("Insira a segunda matriz:");
+            double[][] matrizB = LerMatriz();
+
+            double[][] resultado = Matrix.mult(matrizA, matrizB);
+
+            Console.WriteLine("Resultado:");
+            Console.WriteLine(ImpressorMatricial.ImprimeMatriz<double>(resultado));
+        }
+
+        private static void ProdutoEscalar()
+        {
+            Console.WriteLine("Insira o primeiro vetor:");
+            double[] vetorA = LerVetor();
+
+            Console.WriteLine("Insira o segundo vetor:");
+            double[] vetorB = LerVetor();
+
+            double resultado = Matrix.dot(vetorA, vetorB);
+
+            Console.WriteLine("Resultado: {0}", resultado);
+        }
+        
+        private static double[][] LerMatriz()
+        {
+            Console.Write("Digite o número de linhas da matriz: ");
+            int numLinhas = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o número de colunas da matriz: ");
+            int numColunas = Convert.ToInt32(Console.ReadLine());
+
+            double[][] matriz = Matrix.InicializarMatriz(numLinhas, numColunas);
+
+            for (int i = 0; i < numLinhas; i++)
+            {
+                for (int j = 0; j < numColunas; j++)
+                {
+                    Console.Write("Digite o elemento [{0}][{1}]:", i, j);
+                    matriz[i][j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            return matriz;
+        }
+
+        private static double[] LerVetor()
+        {
+            Console.Write("Digite o número de elementos do vetor: ");
+            int numElementos = Convert.ToInt32(Console.ReadLine());
+
+            double[] vetor = new double[numElementos];
+
+            for (int i = 0; i < numElementos; i++)
+            {
+                Console.WriteLine("Digite o elemento [{0}]: ", i);
+                vetor[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            return vetor;
+        }
+    }
+}
---
+++ Capitulo1/Capitulo1_Biblioteca/Matrix.cs
@@ -0,0 +1,208 @@
+using System;
+
+namespace Capitulo1_Biblioteca
+{
+    /// <summary>
+    /// Classe com métodos estáticos para operações envolvendo matrizes e vetores.
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// Calcula o produto escalar de dois vetores.
+        /// </summary>
+        /// <param name="x">Primeiro vetor.</param>
+        /// <param name="y">Segundo vetor.</param>
+        /// <returns>Produto escalar dos vetores informados.</returns>
+        public static double dot(double[] x, double[] y)
+        {
+            double resultado = 0;
+            
+            for (int i = 0; i < x.Length; i++)
+            {
+                resultado += x[i] * y[i];
+            }
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Calcula o produto matricial de duas matrizes quadradas de mesma ordem.
+        /// </summary>
+        /// <param name="a">Primeira matriz.</param>
+        /// <param name="b">Segunda matriz.</param>
+        /// <returns>Produto de A e B.</returns>
+        /// <exception cref="ArgumentException">Se as matrizes não forem quadradas de mesma ordem.</exception>
+        public static double[][] mult(double[][] a, double[][] b)
+        {
+            int N = a.Length;
+            
+            if (!EhMatrizQuadrada(a, N) || !EhMatrizQuadrada(b, N))
+            {
+                throw new ArgumentException("As matrizes informadas devem ser quadradas e de mesma ordem.");
+            }
+
+            double[][] resultado = InicializarMatriz(N, N);
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        resultado[i][j] += a[i][k] * b[k][j];
+                    }
+                }
+            }
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Valida se a matriz informada é quadrada de ordem N.
+        /// </summary>
+        /// <param name="matriz">Matriz a ser validade.</param>
+        /// <param name="N">Ordem.</param>
+        /// <returns>True caso a matriz seja quadrada de ordem N. False caso contrário.</returns>
+        private static bool EhMatrizQuadrada(double[][] matriz, int N)
+        {
+            if (matriz.Length != N)
+            {
+                return false;
+            }
+            
+            for (int i = 0; i < N; i++)
+            {
+                if (matriz[i].Length != N)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        } 
+        
+        /// <summary>
+        /// Cria uma matriz de double do tamanho informado.
+        /// </summary>
+        /// <param name="numLinhas">Número de linhas da matriz.</param>
+        /// <param name="numColunas">Número de colunas da matriz.</param>
+        /// <returns>Matriz[numLinhas][numColunas]</returns>
+        public static double[][] InicializarMatriz(int numLinhas, int numColunas)
+        {
+            double[][] matriz = new double[numLinhas][];
+            for (int i = 0; i < numLinhas; i++)
+            {
+                matriz[i] = new double[numColunas];
+            }
+
+            return matriz;
+        }
+
+        /// <summary>
+        /// Faz a transposta da matriz informada.
+        /// </summary>
+        /// <param name="a">Matriz cuja tranposta será realizada.</param>
+        /// <returns>Transposta da matriz A.</returns>
+        public static double[][] Transpose(double[][] a)
+        {
+            int numLinhas = a[0].Length;
+            int numColunas = a.Length;
+
+            double[][] transposta = InicializarMatriz(numLinhas, numColunas);
+
+            for (int i = 0; i < numLinhas; i++)
+            {
+                for (int j = 0; j < numColunas; j++)
+                {
+                    transposta[i][j] = a[j][i];
+                }
+            }
+
+            return transposta;
+        }
+        
+        /// <summary>
+        /// Realiza a multiplicação entre uma matriz e um vetor.
+        /// </summary>
+        /// <param name="a">Matriz.</param>
+        /// <param name="x">Vetor.</param>
+        /// <returns>Produto enre a matriz a e o vetor x.</returns>
+        /// <exception cref="ArgumentException">Se a multiplicação não for possível.</exception>
+        public static double[] mult(double[][] a, double[] x)
+        {
+            if (!multiplicacaoEhPossivel(a, x))
+            {
+                throw new ArgumentException("Tamanhos das entradas não são compatíveis.");
+            }
+
+            int tamResultado = a.Length;
+            double[] resultado = new double[tamResultado];
+            
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[0].Length; j++)
+                {
+                    resultado[i] += a[i][j] * x[j];
+                }
+            }
+
+            return resultado;
+        }
+        
+        /// <summary>
+        /// Verifica se a multiplicação entre matriz e vetor é possível.
+        /// </summary>
+        /// <param name="a">Matriz.</param>
+        /// <param name="x">Vetor.</param>
+        /// <returns>True caso a multiplicação seja possível. False caso contrário.</returns>
+        private static bool multiplicacaoEhPossivel(double[][] a, double[] x)
+        {
+            int numColunasA = a[0].Length;
+            int numLinhasX = x.Length;
+
+            return numColunasA == numLinhasX;
+        }
+
+        /// <summary>
+        /// Realiza a multiplicação entre um vetor e uma matriz.
+        /// </summary>
+        /// <param name="y">Vetor.</param>
+        /// <param name="a">Matriz.</param>
+        /// <returns>Produto entre o vetor y e a matriz a.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static double[] mult(double[] y, double[][] a)
+        {
+            if (!multiplicacaoEhPossivel(y, a))
+            {
+                throw new ArgumentException("Tamanhos das entradas não são compatíveis.");
+            }
+
+            int tamResultado = a[0].Length;
+            double[] resultado = new double[tamResultado];
+            
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[0].Length; j++)
+                {
+                    resultado[i] += a[j][i] * y[j];
+                }
+            }
+
+            return resultado;
+        }
+
+        /// <summary>
+        /// Verifica se a multiplicação entre vetor e matriz é possível.
+        /// </summary>
+        /// <param name="y">Vetor.</param>
+        /// <param name="a">Matriz.</param>
+        /// <returns>True caso a multiplicação seja possível. False caso contrário.</returns>
+        private static bool multiplicacaoEhPossivel(double[] y, double[][] a)
+        {
+            int numColunasY = y.Length;
+            int numLinhasA = a.Length;
+
+            return numLinhasA == numColunasY;
+        }
+    }
+}
