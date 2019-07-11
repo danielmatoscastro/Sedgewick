a90c37f8364b24c222a9dfb9129cdac098f0e8b9
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2019-07-11 19:20:28 -0300

    Adição da classe Interval1D

--- Capitulo1/Capitulo1/Exercicio121_Cliente.cs
+++ Capitulo1/Capitulo1/Exercicio121_Cliente.cs
@@ -17,38 +17,15 @@ namespace Capitulo1_Cliente
                 Console.Write("Digite o número de pontos: ");
                 int N = Convert.ToInt32(Console.ReadLine());
 
-                Random random = new Random(DateTime.Now.Millisecond);
                 Point2DBook[] pontos = new Point2DBook[N];
-
-                for (int i = 0; i < pontos.Length; i++)
-                {
-                    pontos[i] = new Point2DBook(random.NextDouble(), random.NextDouble());
-                }
+                
+                GerarPontos(pontos);
 
                 double[][] distancias = Matrix.InicializarMatriz<double>(N, N);
 
-                for (int i = 0; i < pontos.Length - 1; i++)
-                {
-                    for (int j = i; j < pontos.Length; j++)
-                    {
-                        double distancia = pontos[i].DistanceTo(pontos[j]);
-                        distancias[i][j] = distancia;
-                        distancias[j][i] = distancia;
-                    }
-                }
+                CalcularDistancias(pontos, distancias);
 
-                double menorDistancia = distancias[0][1];
-
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        if (i != j && distancias[i][j] < menorDistancia)
-                        {
-                            menorDistancia = distancias[i][j];
-                        }
-                    }
-                }
+                double menorDistancia = EncontraMenorDistancia(distancias, N);
 
                 Console.WriteLine("A menor distância entre dois pontos é {0}", menorDistancia);
 
@@ -62,6 +39,47 @@ namespace Capitulo1_Cliente
                 Console.ReadKey();
             }
 
+        }
+
+        private static double EncontraMenorDistancia(double[][] distancias, int N)
+        {
+            double menorDistancia = distancias[0][1];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i != j && distancias[i][j] < menorDistancia)
+                    {
+                        menorDistancia = distancias[i][j];
+                    }
+                }
+            }
+
+            return menorDistancia;
+        }
+
+        private static void CalcularDistancias(Point2DBook[] pontos, double[][] distancias)
+        {
+            for (int i = 0; i < pontos.Length - 1; i++)
+            {
+                for (int j = i; j < pontos.Length; j++)
+                {
+                    double distancia = pontos[i].DistanceTo(pontos[j]);
+                    distancias[i][j] = distancia;
+                    distancias[j][i] = distancia;
+                }
+            }
+        }
+
+        private static void GerarPontos(Point2DBook[] pontos)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < pontos.Length; i++)
+            {
+                pontos[i] = new Point2DBook(random.NextDouble(), random.NextDouble());
+            }
         }
     }
 }
---
+++ Capitulo1/Capitulo1_Biblioteca/Interval1D.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace Capitulo1_Biblioteca
+{
+    /// <summary>
+    /// Classe que representa um intervalo em uma reta.
+    /// </summary>
+    public class Interval1D
+    {
+        private double left;
+        private double right;
+
+        /// <summary>
+        /// Extremo esquerdo do intervalo.
+        /// </summary>
+        public double Left
+        {
+            get { return left; }
+        }
+
+        /// <summary>
+        /// Extremo direito do intervalo.
+        /// </summary>
+        public double Right
+        {
+            get { return right; }
+        }
+
+        /// <summary>
+        /// Tamanho do intervalo.
+        /// </summary>
+        public double Length
+        {
+            get { return Math.Abs(right - left); }
+        }
+
+        /// <summary>
+        /// Objeto que representa um intervalo em uma reta.
+        /// </summary>
+        /// <param name="left">Extremo esquerdo do intervalo.</param>
+        /// <param name="right">Extremo direito do intervalo.</param>
+        public Interval1D(double left, double right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        /// <summary>
+        /// Verifica se o valor informado está dentro do intervalo.
+        /// </summary>
+        /// <param name="x">Valor a ser verificado.</param>
+        /// <returns>True se x éstá no intervalo. False caso contrário.</returns>
+        public bool Contains(double x)
+        {
+            return left <= x && x <= right;
+        }
+
+        /// <summary>
+        /// Verifica se o intervalo informado tem algum ponto em comum com este intervalo.
+        /// </summary>
+        /// <param name="that">Intervalo a ser verificado.</param>
+        /// <returns>
+        /// True caso esta instância e o intervalo informado tenham algum valor em comum.
+        /// False caso contrário.
+        /// </returns>
+        public bool Intersects(Interval1D that)
+        {
+            return this.Contains(that.left) || this.Contains(that.Right);
+        }
+    }
+}
