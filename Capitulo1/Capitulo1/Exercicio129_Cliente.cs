5c1bd06dc1b48d2dc66d21eea6d18c2b512deb25
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2019-07-14 20:02:50 -0300

    Solução do exercício 1.2.9

--- Capitulo1/Capitulo1/Exercicio129_Cliente.cs
+++ Capitulo1/Capitulo1/Exercicio129_Cliente.cs
@@ -1,7 +1,38 @@
+using System;
+using Capitulo1_Biblioteca;
+
 namespace Capitulo1_Cliente
 {
+    /// <summary>
+    /// Cliente para o exercício 1.2.9.
+    /// </summary>
     public class Exercicio129_Cliente
     {
-        
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = GeradorAleatorio.GerarInteiros(10, 0, 7);
+                Array.Sort(arr);
+
+                Console.WriteLine("Amostra: ");
+                Console.WriteLine(ImpressorVetorial.ImprimeArray<int>(arr));
+
+                for (int i = 0; i <= 7; i++)
+                {
+                    Counter counter = new Counter(String.Format("Buscando {0}", i));
+                    BuscaBinaria.Rank(i, arr, counter);
+                    Console.WriteLine(counter);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
     }
 }
