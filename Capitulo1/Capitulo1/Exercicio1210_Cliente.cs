af1120d62fb5500fe89589e0ea7d75ef4fcaf436
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2019-07-14 21:04:49 -0300

    Solução do exercício 1.2.10

--- Capitulo1/Capitulo1/Exercicio1210_Cliente.cs
+++ Capitulo1/Capitulo1/Exercicio1210_Cliente.cs
@@ -1,7 +1,36 @@
+using System;
+using Capitulo1_Biblioteca;
+
 namespace Capitulo1_Cliente
 {
+    /// <summary>
+    /// Cliente para o exercício 1.2.10.
+    /// </summary>
     public class Exercicio1210_Cliente
     {
-        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Random random = new Random(DateTime.Now.Millisecond);
+                VisualAccumulator visualAccumulator = new VisualAccumulator(2000, 1.0);
+
+                for (int i = 0; i < 2000; i++)
+                {
+                    visualAccumulator.addDataValue(random.NextDouble());
+                }
+                
+                visualAccumulator.ShowGraphics();
+                Console.WriteLine(visualAccumulator);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
     }
 }
