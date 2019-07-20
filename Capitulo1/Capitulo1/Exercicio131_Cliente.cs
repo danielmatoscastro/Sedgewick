493d359407e5344bfd9a8785c49e33e4bc09bfaa
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2019-07-19 21:23:54 -0300

    Solução do exercício 1.3.1

--- Capitulo1/Capitulo1/Exercicio131_Cliente.cs
+++ Capitulo1/Capitulo1/Exercicio131_Cliente.cs
@@ -1,7 +1,41 @@
+using System;
+using Capitulo1_Biblioteca;
+
 namespace Capitulo1_Cliente
 {
+    /// <summary>
+    /// Cliente para o exercício 1.3.1.
+    /// </summary>
     public class Exercicio131_Cliente
     {
-        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite o número de strings: ");
+                int N = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Lendo {0} linhas: ", N);
+                
+                PilhaDeStringsTamanhoFixo pilha = new PilhaDeStringsTamanhoFixo(N);
+                while (!pilha.EstaCheia)
+                {
+                    pilha.Push(Console.ReadLine());
+                }
+                
+                Console.WriteLine("Apresentando as linhas lidas: ");
+                while (!pilha.EstaVazia)
+                {
+                    Console.WriteLine(pilha.Pop());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
     }
 }
