4f612affa7f119e93aa38c51ef855154b49ad543
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2019-07-22 22:45:16 -0300

    Solução para o exercício 1.3.4

---
+++ Capitulo1/Capitulo1/Exercicio134_Cliente.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Capitulo1_Biblioteca;
+
+namespace Capitulo1_Cliente
+{
+    /// <summary>
+    /// Cliente para o exercício 1.3.4.
+    /// </summary>
+    public class Exercicio134_Cliente
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entre com a sequência de parênteses separada por espaços.");
+                string[] parenteses = Console.ReadLine().Trim().Split(new[] {' '}, 
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (ValidaExpressao(parenteses))
+                { 
+                    Console.WriteLine("Expressão é válida.");   
+                }
+                else
+                {
+                    Console.WriteLine("Expressão não é válida.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static bool ValidaExpressao(string[] parenteses)
+        {
+            Dictionary<string, string> tradutor = new Dictionary<string, string>();
+            tradutor.Add("(", ")");
+            tradutor.Add("[", "]");
+            tradutor.Add("{", "}");
+            
+            Pilha<string> pilha = new Pilha<string>();
+
+            foreach (string parentese in parenteses)
+            {
+                if (tradutor.Keys.Contains(parentese))
+                {
+                    pilha.Push(parentese);
+                }
+                else
+                {
+                    if (pilha.estaVazia)
+                    {
+                        return false;
+                    }
+                    
+                    string parenteseEsperado = tradutor[pilha.Pop()];
+                    
+                    if (!parenteseEsperado.Equals(parentese))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return pilha.estaVazia;
+        }
+    }
+}
